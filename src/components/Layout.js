6285e37162fd167064ddf642530f85e1033253c0
umutural02 <umutural1596@gmail.com>
2024-07-14 18:47:25 +0300

    Add Layout and Header components.

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+
+function Layout({ children }) {
+  
+    return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
+import Layout from './components/Layout';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,7 +11,9 @@ import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
     <ToastContainer />
   </React.StrictMode>
 );
