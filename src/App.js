b060d2ff194a19c830b852592917edee8b5385f9
umutural02 <umutural1596@gmail.com>
2024-07-14 19:43:04 +0300

    Add React Router and Characters Page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import { useEffect, useState } from "react";
-import { notifyError } from "./services/NotificationService";
-import { get } from "./services/ApiService";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { themeChange } from 'theme-change'
 
-
+import Home from "./pages/Home";
+import Characters from "./pages/Characters";
 
 function App() {
-
-  const [data, setData] = useState(null);
 
   useEffect(() => {
     themeChange(false)
   }, [])
 
-  useEffect(() => {
-
-    const fetchData = async () => {
-      const errorMessage = "Error fetching character";
-      const responseData = await get("/character/1", errorMessage);
-      
-      if (responseData) {
-        setData(responseData);
-      }
-    };
-
-    fetchData();
-
-  }, []);
-
   return (
-    <div className="App">
-      <button className="btn" onClick={() => notifyError("Yeah")}>Button</button>
-      {
-        data &&
-        <p>{data.name}</p>
-      }
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/characters" element={<Characters />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Characters.js
@@ -0,0 +1,9 @@
+function Characters() {
+    return (
+        <div>
+            <h1>Characters</h1>
+        </div>
+    )
+}
+
+export default Characters;
